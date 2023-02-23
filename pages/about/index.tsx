ba2e2c1d7421b7258279ad87f39e9048673d0f31
Luciano275 <luna80153@gmail.com>
2023-02-23 20:57:11 -0300

    init

---
+++ pages/about/index.tsx
@@ -0,0 +1,21 @@
+import styles from '@/styles/Home.module.css';
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About page</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <p>
+            About Page
+          </p>
+        </div>
+      </main>
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
+import styles from '@/styles/Home.module.css'
+import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,10 +17,9 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
+          <Link href='/about'>
+            About page
+          </Link>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
